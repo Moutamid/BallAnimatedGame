4711d0806c497b0a3dee8926267f332e6b764c27
Moutamid <lumianokia188@gmail.com>
2022-12-03 20:47:57 +0500

    initial commit

--- app/src/main/java/com/moutamid/ballanimatedgame/MainActivity.java
+++ app/src/main/java/com/moutamid/ballanimatedgame/MainActivity.java
@@ -14,6 +14,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -29,7 +30,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public static int x = 0;
     public static int y = 0;
     public static int z = 0;
-    private TextView xTxt,yTxt,zTxt;
+    private TextView xTxt, yTxt, zTxt;
+
+    int screenwidth;
+    int screenheight;
+    int maxValueW;
+    int maxValueH;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -40,42 +46,53 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         xTxt = findViewById(R.id.x_axis);
         yTxt = findViewById(R.id.y_axis);
         zTxt = findViewById(R.id.z_axis);
-     //   requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //   requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        Bitmap myBitmap = BitmapFactory.decodeResource(getResources(),R.drawable.ball);
+        Bitmap myBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
         Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
         Canvas canvas = new Canvas(tempBitmap);
         Drawable drawable = imageView.getDrawable();
-        drawable.setBounds(20, 30, drawable.getIntrinsicWidth()+20, drawable.getIntrinsicHeight()+30);
+        drawable.setBounds(20, 30, drawable.getIntrinsicWidth() + 20, drawable.getIntrinsicHeight() + 30);
         drawable.draw(canvas);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        screenwidth = metrics.widthPixels;
+        screenheight = metrics.heightPixels;
+
+        maxValueW = screenwidth - 281;
+        maxValueH = screenheight - 281;
+
+        Log.d("TAG", "onCreate: " + screenwidth + "," + screenheight);
+        Log.d("TAG", "onCreateBitmap: " + myBitmap.getWidth() + "," + myBitmap.getHeight());
+
+        //1080,2177
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            Log.d("TAG", "onCreateValues: " +event.values[0]);
+            Log.d("TAG", "onCreateValues: " + event.values[1]);
+
             x -= (int) event.values[0];
             y += (int) event.values[1];
             z += (int) event.values[2];
 
+            xTxt.setText("x = " + x);
+            yTxt.setText("y = " + y);
+            zTxt.setText("z = " + z);
 
-            xTxt.setText("x = "+ x);
-            yTxt.setText("y = "+ y);
-            zTxt.setText("z = "+ z);
+            if (y < maxValueH && y > 0)
+                imageView.setY(y);
+            if (x < maxValueW && x > 0)
+                imageView.setX(x);
 
-            DisplayMetrics metrics = new DisplayMetrics();
-            getWindowManager().getDefaultDisplay().getMetrics(metrics);
-            int screenwidth = metrics.widthPixels;
-            int screenheight = metrics.heightPixels;
-
-            //Toast.makeText(MainActivity.this,""+screenwidth + "," + screenheight,Toast.LENGTH_SHORT).show();
-
-            
-            imageView.setY(y);
-            imageView.setX(x);
             imageView.setZ(z);
         }
     }
